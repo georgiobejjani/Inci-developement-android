a783fed1eaea7545e1f6ec2c807ddc62e09b315e
georgio <georgio.bejjani@hotmail.com>
2021-09-19 20:46:12 +0300

    roll all button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,6 +14,7 @@ import android.graphics.drawable.BitmapDrawable
 
 
 class MainActivity : AppCompatActivity() {
+    var nbdice:Int=1
     var count = 1
 //    var emptydice =findViewById<ImageView>(R.id.emptydice)
 //    var emptydice2 =findViewById<ImageView>(R.id.emptydice2)
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         imagesArray.add(findViewById<ImageView>(R.id.emptydice2))
         imagesArray.add(findViewById<ImageView>(R.id.emptydice3))
         imagesArray.add(findViewById<ImageView>(R.id.emptydice4))
+        nbdice=findViewById<TextView>(R.id.diceCounter).text.toString().toInt()
     }
     fun randomnumber():String
     {
@@ -76,89 +78,17 @@ class MainActivity : AppCompatActivity() {
 
 
         fun btnSetClicked(sender: View) {
-//        var mtv = findViewById<TextView>(R.id.dice1)
-//        var mtv2 = findViewById<TextView>(R.id.dice2)
-//        var mtv3 = findViewById<TextView>(R.id.dice3)
-//        var mtv4 = findViewById<TextView>(R.id.dice4)
 
-//        var emptydice =findViewById<ImageView>(R.id.emptydice)
-//        var emptydice2 =findViewById<ImageView>(R.id.emptydice2)
-//        var emptydice3 =findViewById<ImageView>(R.id.emptydice3)
-//        var emptydice4 =findViewById<ImageView>(R.id.emptydice4)
-//
-//        val numb1 = randomnumber()
-//        val numb2 = randomnumber()
-//        val numb3 = randomnumber()
-//        val numb4 = randomnumber()
-//
-//
-//       val dice1 = when(numb1.toInt())
-//        {
-//            1-> R.drawable.dice1
-//            2-> R.drawable.dice2
-//            3-> R.drawable.dice3
-//            4-> R.drawable.dice4
-//            5-> R.drawable.dice5
-//            else-> R.drawable.dice6
-//       }
-//        emptydice.setImageResource(dice1)
-//        if(emptydice2.visibility==View.VISIBLE)
-//        {
-//            val dice2 = when(numb2.toInt())
-//            {
-//                1-> R.drawable.dice1
-//                2-> R.drawable.dice2
-//                3-> R.drawable.dice3
-//                4-> R.drawable.dice4
-//                5-> R.drawable.dice5
-//                else-> R.drawable.dice6
-//            }
-//            emptydice2.setImageResource(dice2)
-//        }
-//
-//        if(emptydice3.visibility==View.VISIBLE)
-//        {
-//            val dice3 = when(numb3.toInt())
-//            {
-//                1-> R.drawable.dice1
-//                2-> R.drawable.dice2
-//                3-> R.drawable.dice3
-//                4-> R.drawable.dice4
-//                5-> R.drawable.dice5
-//                else-> R.drawable.dice6
-//            }
-//            emptydice3.setImageResource(dice3)
-//        }
-//
-//        if(emptydice4.visibility==View.VISIBLE)
-//        {
-//            val dice4 = when(numb4.toInt())
-//            {
-//                1-> R.drawable.dice1
-//                2-> R.drawable.dice2
-//                3-> R.drawable.dice3
-//                4-> R.drawable.dice4
-//                5-> R.drawable.dice5
-//                else-> R.drawable.dice6
-//            }
-//            emptydice4.setImageResource(dice4)
-//        }
 
-//        if(mtv2.visibility == View.VISIBLE)
-//        {
-//           var text2 = numb2.toString()
-//           mtv2.text=text2
-//        }
-//         if(mtv3.visibility == View.VISIBLE)
-//        {
-//            var text3 = numb3.toString()
-//            mtv3.text=text3
-//        }
-//        if(mtv4.visibility == View.VISIBLE)
-//        {
-//            var text4 = numb4.toString()
-//            mtv4.text=text4
-//        }
+            for(i in 0 until nbdice)
+            {
+
+                    imagesArray[i].setImageResource(randomimage())
+            }
+            for (i in nbdice until 4)
+            {
+                imagesArray[i].setImageResource(R.drawable.emptydice)
+            }
     }
 
         fun btnAddDice(sender: View) {
@@ -166,6 +96,7 @@ class MainActivity : AppCompatActivity() {
         var emptydice2 =findViewById<ImageView>(R.id.emptydice2)
         var emptydice3 =findViewById<ImageView>(R.id.emptydice3)
         var emptydice4 =findViewById<ImageView>(R.id.emptydice4)
+
 
         var mtv = findViewById<TextView>(R.id.diceCounter)
         count++
@@ -198,6 +129,7 @@ class MainActivity : AppCompatActivity() {
                 emptydice4.setImageResource(R.drawable.emptydice)
             }
         }
+            nbdice=findViewById<TextView>(R.id.diceCounter).text.toString().toInt()
 
     }
 
@@ -208,6 +140,7 @@ class MainActivity : AppCompatActivity() {
             var emptydice2 =findViewById<ImageView>(R.id.emptydice2)
             var emptydice3 =findViewById<ImageView>(R.id.emptydice3)
             var emptydice4 =findViewById<ImageView>(R.id.emptydice4)
+            nbdice=findViewById<TextView>(R.id.diceCounter).text.toString().toInt()
 
             var mtv = findViewById<TextView>(R.id.diceCounter)
             count--
@@ -255,8 +188,6 @@ class MainActivity : AppCompatActivity() {
 
         fun rollnextdice(Sender: View)
         {
-            var nbdice=findViewById<TextView>(R.id.diceCounter).text.toString().toInt()
-
             var ghe= (imagesArray[nbdice-1].getDrawable() as BitmapDrawable).bitmap
             if(!ghe.sameAs((getDrawable(R.drawable.emptydice) as BitmapDrawable).bitmap))
             {
@@ -267,16 +198,12 @@ class MainActivity : AppCompatActivity() {
             }
             for(i in 0 until nbdice)
             {
-//                Log.d("georgio",imageviewarray[i].drawable.toString() )
-//                Log.d("georgio2",getDrawable(R.drawable.emptydice).toString())
                 var gh= (imagesArray[i].getDrawable() as BitmapDrawable).bitmap
                 if(gh.sameAs((getDrawable(R.drawable.emptydice) as BitmapDrawable).bitmap))
                 {
-                    //put new image
                     imagesArray[i].setImageResource(randomimage())
                     return
                 }
-
             }
         }
 }
